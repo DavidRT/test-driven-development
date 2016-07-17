96bc7ff8e43d5db434f9df75441007c557a62661
David Rodriguez <drodtach@gmail.com>
2016-07-17 17:33:27 -0300

    Complete chapter 9, implement currency

--- MoneyExample/src/main/java/com/davidrt/Money.java
+++ MoneyExample/src/main/java/com/davidrt/Money.java
@@ -3,13 +3,19 @@ package com.davidrt;
 abstract class Money {
 
 	protected int amount;
+	protected String currency;
+	
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 	
 	public static Money dollar(int amount){
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	
 	public static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount,"CHF");
 	}
 	
 	abstract Money times(int multiplier);
@@ -21,6 +27,8 @@ abstract class Money {
 				getClass().equals(money.getClass());
 	}
 
-	
+	public String currency(){
+		return this.currency;
+	}
 	
 }

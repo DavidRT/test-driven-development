a5eccb5d9e0e8c0dc3cfbe26cc007f6e6171e765
David Rodriguez <drodtach@gmail.com>
2016-07-17 18:11:08 -0300

    Add missing classes

---
+++ MoneyExample/src/main/java/com/davidrt/Bank.java
@@ -0,0 +1,8 @@
+package com.davidrt;
+
+public class Bank {
+
+	Money reduce(Expression source, String to){
+		return Money.dollar(10);
+	}
+}
---
+++ MoneyExample/src/main/java/com/davidrt/Expression.java
@@ -0,0 +1,6 @@
+package com.davidrt;
+
+public interface Expression {
+	
+
+}

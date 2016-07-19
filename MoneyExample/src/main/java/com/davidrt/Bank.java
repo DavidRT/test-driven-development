67ba134aa72bd2942b19a43a2f5d5f011e72555e
David Rodriguez <drodtach@gmail.com>
2016-07-19 00:10:32 -0300

    Complete chapter 15

--- MoneyExample/src/main/java/com/davidrt/Bank.java
+++ MoneyExample/src/main/java/com/davidrt/Bank.java
@@ -1,8 +1,21 @@
 package com.davidrt;
 
-public class Bank {
+import java.util.Hashtable;
 
+public class Bank {
+	
+	private Hashtable rates = new Hashtable();
+
+	int rate(String from, String to){
+		if ( from.equals(to)) return 1;
+		Integer rate = (Integer) rates.get(new Pair(from,to));
+		return rate.intValue();
+	}
 	Money reduce(Expression source, String to){
-		return source.reduce(to);
+		return source.reduce(this,to);
+	}
+	
+	void addRate(String from, String to, int rate){
+		rates.put(new Pair(from,to), new Integer(rate));
 	}
 }
--- MoneyExample/src/main/java/com/davidrt/Money.java
+++ MoneyExample/src/main/java/com/davidrt/Money.java
@@ -30,8 +30,9 @@ package com.davidrt;
 		return new Sum(this,addend);
 	}
 	
-	public Money reduce(String to){
-		return this;
+	public Money reduce(Bank bank,String to){
+		int rate = bank.rate(currency, to);
+		return new Money(amount/rate, to);
 	}
 	
 	@Override
